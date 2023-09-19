f98918ad878b19699635f4783059ec23637354a7
Iam-Dd0t <ddotextra@gmail.com>
2023-09-19 07:17:29 -0400

    Added badge for GitHub Actions

--- service/routes.py
+++ service/routes.py
@@ -69,8 +69,7 @@ def list_accounts():
         app.logger.info("Request to list Accounts")
 
         accounts = Account.all()
-        account_list = [account.serialize() for account in accounts]
-        
+        account_list = [account.serialize() for account in accounts]        
         app.logger.info("Returning [%s] accounts", len(account_list))
         return jsonify(account_list), status.HTTP_200_OK
 
@@ -89,8 +88,6 @@ def get_accounts(account_id):
         if not account:
                abort(status.HTTP_404_NOT_FOUND, f"Account with id [{account_id}] could not be found.")
         return account.serialize(), status.HTTP_200_OK
-
-
 
 ######################################################################
 # UPDATE AN EXISTING ACCOUNT
@@ -125,8 +122,6 @@ def delete_accounts(account_id):
         account = Account.find(account_id)
         if account:
              account.delete()
-
-
         return "", status.HTTP_204_NO_CONTENT
 
 
